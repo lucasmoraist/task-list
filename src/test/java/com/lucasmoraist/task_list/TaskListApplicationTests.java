f1772e20495184c785d126229e0747764caa240e
lucasmoraist <luksmnt1101@gmail.com>
2024-08-01 16:49:50 -0300

    chore: Add initial project files and configurations

---
+++ src/test/java/com/lucasmoraist/task_list/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.task_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
