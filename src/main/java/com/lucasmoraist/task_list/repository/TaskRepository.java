e3c7ffff63c66926834f32e9334726a0bb75cfd5
lucasmoraist <luksmnt1101@gmail.com>
2024-08-02 11:30:27 -0300

    feat: Add @Repository annotation to TaskRepository

--- src/main/java/com/lucasmoraist/task_list/repository/TaskRepository.java
+++ src/main/java/com/lucasmoraist/task_list/repository/TaskRepository.java
@@ -4,7 +4,9 @@ import com.lucasmoraist.task_list.model.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByTitleContaining(String title, Pageable pageable);
 }
