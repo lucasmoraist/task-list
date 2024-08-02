aefaa955061e7e4c40dc4e886cc8c3249d98b2a9
lucasmoraist <luksmnt1101@gmail.com>
2024-08-02 15:15:03 -0300

    feat: Add UpdateStatusTaskController for updating task status

---
+++ src/main/java/com/lucasmoraist/task_list/controller/task/UpdateStatusTaskController.java
@@ -0,0 +1,54 @@
+package com.lucasmoraist.task_list.controller.task;
+
+import com.lucasmoraist.task_list.exceptions.TaskNotFound;
+import com.lucasmoraist.task_list.model.Task;
+import com.lucasmoraist.task_list.model.dto.TaskResponse;
+import com.lucasmoraist.task_list.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tasks")
+@Tag(name = "Task")
+public class UpdateStatusTaskController {
+
+    @Autowired
+    private TaskService service;
+
+    @Operation(summary = "Update task status", description = "Update the status of a task by its id")
+    @Parameter(name = "id", description = "Task id", required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task status updated successfully", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = TaskResponse.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = TaskNotFound.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(example = """
+                            {
+                            	"msg": "Fill in all mandatory fields",
+                            	"status": "BAD_REQUEST"
+                            }
+                            """)
+            ))
+    })
+    @PatchMapping("status/{id}")
+    public ResponseEntity<TaskResponse> updateTitle(@PathVariable Long id, @Valid @RequestBody Task task){
+        TaskResponse response = this.service.updateStatusTask(id, task);
+        return ResponseEntity.ok().body(response);
+    }
+
+}
