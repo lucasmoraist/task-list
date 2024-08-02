8830bbe5aa7ade1360ca733f9a44ab036f2d9e64
lucasmoraist <luksmnt1101@gmail.com>
2024-08-02 09:26:56 -0300

    feat: Add pagination support to TaskRepository

--- src/main/java/com/lucasmoraist/task_list/repository/TaskRepository.java
+++ src/main/java/com/lucasmoraist/task_list/repository/TaskRepository.java
@@ -1,7 +1,10 @@
 package com.lucasmoraist.task_list.repository;
 
 import com.lucasmoraist.task_list.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Page<Task> findByTitleContaining(String title, Pageable pageable);
 }
