a6ddd931520c9d8f25e272f9ebbb57b187e67d3d
lucasmoraist <luksmnt1101@gmail.com>
2024-08-04 23:28:12 -0300

    refactor: Update SecurityConfig to allow access to root ("/") path

--- src/main/java/com/lucasmoraist/task_list/infra/security/SecurityConfig.java
+++ src/main/java/com/lucasmoraist/task_list/infra/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
