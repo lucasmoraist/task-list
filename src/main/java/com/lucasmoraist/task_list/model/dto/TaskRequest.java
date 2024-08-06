f6eef40120610243a3285fb7ebe781b25141017d
lucasmoraist <luksmnt1101@gmail.com>
2024-08-06 11:17:31 -0300

    feat: Add status field to TaskRequest

--- src/main/java/com/lucasmoraist/task_list/model/dto/TaskRequest.java
+++ src/main/java/com/lucasmoraist/task_list/model/dto/TaskRequest.java
@@ -1,4 +1,6 @@
 package com.lucasmoraist.task_list.model.dto;
 
-public record TaskRequest(String title, String description) {
+import com.lucasmoraist.task_list.model.StatusType;
+
+public record TaskRequest(String title, String description, StatusType status) {
 }
