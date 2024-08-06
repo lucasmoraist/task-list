20d4e923520c733e431ba13328fb662ef82b7b02
lucasmoraist <luksmnt1101@gmail.com>
2024-08-06 11:18:24 -0300

    refactor: EmailService to use Task object in sendEmail method

--- src/main/java/com/lucasmoraist/task_list/service/TaskService.java
+++ src/main/java/com/lucasmoraist/task_list/service/TaskService.java
@@ -20,6 +20,14 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Service class for Task entity.
+ * This class is responsible for handling the business logic of the application.
+ * It is responsible for creating, updating, listing and deleting tasks.
+ * It also sends emails to the user when a task is created, updated or deleted.
+ *
+ * @author lucasmoraist
+ */
 @Service
 @CacheConfig(cacheNames = {"tasks"})
 public class TaskService {
@@ -33,11 +41,24 @@ public class TaskService {
         this.repository = repository;
     }
 
+    /**
+     * Creates a new task and saves it to the database.
+     *
+     * @param request TaskRequest object containing the task information.
+     * @return TaskResponse object containing the ID of the created task.
+     */
     public TaskResponse createTask(TaskRequest request) {
         Task task = this.create(request);
         return new TaskResponse(task.getId());
     }
 
+    /**
+     * Lists all tasks in the database.
+     *
+     * @param sortBy Field to sort the tasks by.
+     * @param order  Order to sort the tasks by.
+     * @return List of tasks sorted by the specified field and order.
+     */
     public List<Task> listTaskSortBy(String sortBy, String order) {
         Sort sort = order.equalsIgnoreCase(Sort.Direction.ASC.name()) ?
                 Sort.by(Sort.Order.asc(sortBy)) :
@@ -45,9 +66,14 @@ public class TaskService {
         return this.repository.findAll(sort);
     }
 
-    @Caching(
-            cacheable = @Cacheable(value = "tasks", key = "#status + '-' + #pageable.pageNumber + '-' + #pageable.pageSize")
-    )
+    /**
+     * Lists all tasks in the database with a specific status.
+     *
+     * @param status   Status to filter the tasks by.
+     * @param pageable Pageable object containing the page number and size.
+     * @return Page object containing the tasks with the specified status.
+     */
+    @Cacheable(value = "tasks", key = "#status + '-' + #pageable.pageNumber + '-' + #pageable.pageSize")
     public Page<EntityModel<Task>> listTaskPage(StatusType status, Pageable pageable) {
         logger.info("Listing tasks with status: {}", status);
         Page<Task> tasks = (status == null) ?
@@ -57,6 +83,12 @@ public class TaskService {
         return tasks.map(Task::toEntityModel);
     }
 
+    /**
+     * Finds a task in the database by its ID.
+     *
+     * @param id ID of the task to be found.
+     * @return Task object with the specified ID.
+     */
     @Cacheable(value = "tasks", key = "#id")
     public Task findTask(Long id) {
         logger.info("Finding task with ID: {} from database", id);
@@ -67,66 +99,97 @@ public class TaskService {
                 });
     }
 
+    /**
+     * Finds all tasks in the database with a specific status.
+     *
+     * @param status Status to filter the tasks by.
+     * @return List of tasks with the specified status.
+     */
     public List<Task> findTasksByStatus(StatusType status) {
         logger.info("Finding tasks with status: {}", status);
         return this.repository.findTaskByStatus(status);
     }
 
-    @Caching(
-            put = @CachePut(value = "tasks", key = "#id")
-    )
+    /**
+     * Updates the title of a task in the database.
+     *
+     * @param id      ID of the task to be updated.
+     * @param request TaskRequest object containing the new title.
+     * @return TaskResponse object containing the ID of the updated task.
+     */
+    @CachePut(value = "tasks", key = "#id")
     public TaskResponse updateTitleTask(Long id, TaskRequest request) {
-        logger.info("Updating title of task with ID: {}", id);
         Task task = this.findTask(id);
-        task.setTitle(request.title());
-        this.repository.save(task);
-
-        logger.info("Sending email for task title update: {}", task.getTitle());
-        this.emailService.sendEmailToUpdatingTask(task.getTitle(), task.getDescription(), task.getStatus(), task.getUpdatedAt());
-        return new TaskResponse(task.getId());
+        return this.update("title", task, request);
     }
 
-    @Caching(
-            put = @CachePut(value = "tasks", key = "#id")
-    )
+    /**
+     * Updates the description of a task in the database.
+     *
+     * @param id      ID of the task to be updated.
+     * @param request TaskRequest object containing the new description.
+     * @return TaskResponse object containing the ID of the updated task.
+     */
+    @CachePut(value = "tasks", key = "#id")
     public TaskResponse updateDescriptionTask(Long id, TaskRequest request) {
-        logger.info("Updating description of task with ID: {}", id);
         Task task = this.findTask(id);
-        task.setDescription(request.description());
-        this.repository.save(task);
-
-        logger.info("Sending email for task description update: {}", task.getTitle());
-        this.emailService.sendEmailToUpdatingTask(task.getTitle(), task.getDescription(), task.getStatus(), task.getUpdatedAt());
-        return new TaskResponse(task.getId());
+        return this.update("description", task, request);
     }
 
-    @Caching(
-            put = @CachePut(value = "tasks", key = "#id")
-    )
-    public TaskResponse updateStatusTask(Long id, Task task) {
-        logger.info("Updating status of task with ID: {}", id);
-        Task taskToUpdate = this.findTask(id);
-        taskToUpdate.setStatus(task.getStatus());
-        this.repository.save(taskToUpdate);
-
-        logger.info("Sending email for task status update: {}", taskToUpdate.getTitle());
-        this.emailService.sendEmailToUpdatingTask(taskToUpdate.getTitle(), taskToUpdate.getDescription(), taskToUpdate.getStatus(), taskToUpdate.getUpdatedAt());
-        return new TaskResponse(taskToUpdate.getId());
+    /**
+     * Updates the status of a task in the database.
+     *
+     * @param id      ID of the task to be updated.
+     * @param request TaskRequest object containing the new status.
+     * @return TaskResponse object containing the ID of the updated task.
+     */
+    @CachePut(value = "tasks", key = "#id")
+    public TaskResponse updateStatusTask(Long id, TaskRequest request) {
+        Task task = this.findTask(id);
+        return this.update("status", task, request);
     }
 
-    @Caching(
-            evict = @CacheEvict(value = "tasks", key = "#id")
-    )
+    /**
+     * Deletes a task from the database.
+     *
+     * @param id ID of the task to be deleted.
+     */
+    @CacheEvict(value = "tasks", key = "#id")
     public void deleteTask(Long id) {
         logger.info("Deleting task with ID: {}", id);
         Task task = this.findTask(id);
-        this.emailService.sendEmailToDeletingTask(task.getTitle(), task.getDescription(), task.getStatus(), LocalDateTime.now());
+        this.emailService.sendEmail("deleting", task, LocalDateTime.now());
         this.repository.delete(task);
         logger.info("Task with ID: {} deleted successfully", id);
     }
 
+    private TaskResponse update(String update, Task task, TaskRequest request) {
+        switch (update) {
+            case "title":
+                logger.info("Updating title of task with ID: {}", task.getId());
+                task.setTitle(request.title());
+                this.repository.save(task);
+
+                logger.info("Sending email for task title update: {}", task.getTitle());
+            case "description":
+                logger.info("Updating description of task with ID: {}", task.getId());
+                task.setDescription(request.description());
+                this.repository.save(task);
+
+                logger.info("Sending email for task description update: {}", task.getTitle());
+            case "status":
+                logger.info("Updating status of task with ID: {}", task.getId());
+                task.setStatus(request.status());
+                this.repository.save(task);
+
+                logger.info("Sending email for task status update: {}", task.getTitle());
+        }
+        this.emailService.sendEmail("updating", task, task.getUpdatedAt());
+        return new TaskResponse(task.getId());
+    }
+
     @CachePut(value = "tasks", key = "#result.id")
-    private Task create(TaskRequest request){
+    private Task create(TaskRequest request) {
         logger.info("Creating new task with title: {}", request.title());
         Task task = new Task(request);
         this.repository.save(task);
@@ -134,7 +197,7 @@ public class TaskService {
         logger.info("Sending email for task creation: {}", task.getTitle());
 
         try {
-            this.emailService.sendEmailToCreatingTask(task.getTitle(), task.getDescription(), task.getStatus(), task.getCreatedAt());
+            this.emailService.sendEmail("creating", task, task.getCreatedAt());
         } catch (MailAuthenticationException e) {
             logger.error("Error sending email: {}", e.getMessage());
             throw new SendMailException();
