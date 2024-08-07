e1c9dc75909f931ed1b277f65ed2f805b3ee022c
lucasmoraist <luksmnt1101@gmail.com>
2024-08-07 14:24:29 -0300

    chore: Add security requirement for bearer token in SwaggerConfig

--- src/main/java/com/lucasmoraist/task_list/infra/documentation/SwaggerConfig.java
+++ src/main/java/com/lucasmoraist/task_list/infra/documentation/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +23,8 @@ public class SwaggerConfig {
                         .contact(contact())
                         .license(license())
                 )
-                .components(components());
+                .components(components())
+                .addSecurityItem(new SecurityRequirement().addList("bearer"));
     }
 
     private Contact contact(){
